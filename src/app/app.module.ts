5d1b8ce196bd240a4624ab55411d08126f484660
BVDD <bart.vandendriessche2@gmail.com>
2020-01-04 10:47:16 +0100

    - Added EventModule and associated components - Added LoginModule and associated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventComponentComponent } from './event-component/event-component.component';
+import { EventDetailComponent } from './event/event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponentComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule
