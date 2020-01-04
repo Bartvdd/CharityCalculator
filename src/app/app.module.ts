284a28cd14c75c590c8c78cc6813711ca5096100
BVDD <bart.vandendriessche2@gmail.com>
2020-01-04 10:54:55 +0100

    - Correction after renaming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EventComponentComponent } from './event-component/event-component.component';
+import { EventComponentComponent } from './event/event.component';
 import { EventDetailComponent } from './event/event-detail/event-detail.component';
 
 @NgModule({
